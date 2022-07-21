8b22519f5576aa04d580f478189850a935865546
haldeg01 <haldeg01@KF5CG9023G9R.kits.kfplc.com>
2022-07-21 21:26:18 +0100

    adding specialty and added to Vet, closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
 
+public class Vet extends Person{
+	private Set<Specialty> specialty;
+
+	public Set<Specialty> getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(Set<Specialty> specialty) {
+		this.specialty = specialty;
+	}
+	
+	
 }
  
