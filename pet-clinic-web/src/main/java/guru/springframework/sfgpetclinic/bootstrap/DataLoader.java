f421120f04177130fb7172ad916b608cd1d1a29e
haldeg01 <haldeg01@KF5CG9023G9R.kits.kfplc.com>
2022-07-22 18:59:22 +0100

    Add PetType in Bootstrap, closes #33

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Component
@@ -14,18 +16,27 @@ public class DataLoader implements CommandLineRunner{
 	
 	private final OwnerService ownerService;
 	private final VetService vetService;
-	/*
-	 * public DataLoader() { super(); ownerService = new OwnerMapService();
-	 * vetService = new VetMapService(); // TODO Auto-generated constructor stub }
-	 */
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	private final PetTypeService petTypeService;
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		super();
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		PetType savedCatPetType = petTypeService.save(cat);
+		
+		
 		Owner owner1 =new Owner();
 		owner1.setFirstName("Bombie");
 		owner1.setLastName("Halder");
@@ -51,6 +62,7 @@ public class DataLoader implements CommandLineRunner{
 		System.out.println("Loading Vets......");
 		
 	}
+	
 
 	
 
