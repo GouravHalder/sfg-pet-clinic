e7744bc88bc6a292367ff5c6203cbbd91a4f401a
haldeg01 <haldeg01@KF5CG9023G9R.kits.kfplc.com>
2022-08-05 21:03:03 +0100

    Fake comit

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -12,7 +12,7 @@ public class BaseEntity implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id; 
 
 	public Long getId() {
 		return id;
